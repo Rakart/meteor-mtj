53238feb8240b72aaa93fa60f1c69b34951deafe
Rakart <carlos.greblo@gmail.com>
2015-08-10 13:22:41 +0800

    Edit-Profile
    Added styles to edit tutor page

--- core/client/controllers/home.client.controller.js
+++ core/client/controllers/home.client.controller.js
@@ -2,7 +2,6 @@ angular.module('core').controller('HomeController', ['$scope', '$meteor',
     function($scope, $meteor) {
 
 		$scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
-		
 		$scope.thisUser = $meteor.object(Meteor.users, { _id: Meteor.userId() }, false).subscribe('thisUser');
 
 		console.log($scope.thisUser);
